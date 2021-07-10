d8d06739d75fe65d2a99f62eb0e4bb5743679041
Emiliano <galeanaara@ciencias.unam.mx>
2021-07-10 16:26:35 -0500

    update ServiceApplication main

--- src/main/java/com/invoiceservice/InvoiceServiceApplication.java
+++ src/main/java/com/invoiceservice/InvoiceServiceApplication.java
@@ -2,12 +2,18 @@ package com.invoiceservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class InvoiceServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(InvoiceServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(InvoiceServiceApplication.class, args);
+    }
 
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
